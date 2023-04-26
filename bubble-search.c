d77ba56e14a839787e54fb7d3ed5cd081b613d1b
mitali1407 <110607289+mitali1407@users.noreply.github.com>
2023-04-26 11:06:33 +0530

    Add files via upload

---
+++ bubble-search.c
@@ -0,0 +1,34 @@
+#include <windows.h>
+
+#include<stdio.h>
+void Bubblesort(int [],int);
+
+int main()
+{
+	int arr[5]={-2,45,0,11,-9};
+	printf("the sorted list is:\n");
+    Bubblesort(arr,5);
+    return 0;
+
+}
+void Bubblesort(int arr[],int n)
+{
+	int pass=n-1;
+	int comparisons=n-1;
+	int i,j,k,temp;
+	for(i=0;i<=pass;i++)
+	{
+		for(j=0;j<=comparisons-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				temp=arr[j+1];
+				arr[j+1]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	for (k=0;k<n;k++)
+	{
+		printf("%d ",arr[k]);
+	}
